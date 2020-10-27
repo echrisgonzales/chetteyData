3084eb0c939a04e286e53b321154059370e96896
Chris Gonzales <echrisgonzales@gmail.com>
2020-10-26 20:35:11 -0500

    line change

---
+++ association.R
@@ -0,0 +1,25 @@
+No Association (0-.19)
+-type
+-scorecard_rej_rate
+-ipeds_enrollment_2000
+-endowment_pc_2000
+
+Weak(.2-.29)
+-region (neg)
+-black_share_fall_2000
+
+Moderate(.3-.49)
+-sat_avg_2001
+-exp_instr_pc_2000
+
+Strong(.5+)
+-tier (neg)
+-iclevel(neg)
+-par_median
+-barrons(neg)
+-scorecard_median_earnings_2011
+-avgfascal_2001
+-par_q5
+-par_q1(neg)
+
+
